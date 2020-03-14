9a435c7e068be3f182a15e340c9f95d2f7b32ec3
[12NaN] <frank.orefice25@students.kbcc.cuny.edu>
2020-03-14 00:15:37 -0400

    Added comments to explain the code

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 # main.py
-import sys
+import sys # Used to be able to pass CLI arguments to mapping
 
-def mapping(a, b):
-    if(len(a) != len(b)):
-        return "false"
-    dicts = {}
-    for i in range(len(a)):
-        if(a[i] not in dicts):
+def mapping(a, b):          # Takes two inputs
+    if(len(a) != len(b)):   # if 'a' and 'b' don't have the same length you can't map each value 1-to-1
+        return "false"      # thus return false
+    dicts = {}              
+    for i in range(len(a)):     # Since 'a' and 'b' are the same length iterate through the length of 'a'
+        if(a[i] not in dicts):  # If the value in 'a' at index 'i' is not in the dictionary, then map the value at b[i] to it
             dicts[a[i]] = b[i];
-        else:
+        else:                   # Otherwise, return false since it can't be 1-to-1
             return "false"
 
-    return "true"
+    return "true"               # If each value in 'a' is mapped to a value in 'b', then return true
 
 if __name__ == "__main__":
-    print(mapping(sys.argv[1], sys.argv[2]));
+    print(mapping(sys.argv[1], sys.argv[2])); # Passes CLI arguments to mapping 
