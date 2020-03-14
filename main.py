f401f0f649f2070a00a914e60ce890e5c82fffe5
[12NaN] <frank.orefice25@students.kbcc.cuny.edu>
2020-03-13 20:41:01 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# main.py
+
+def mapping(a, b):
+    if(len(a) != len(b)):
+        return false
+    
+
+    return true
+
+def main():
+    a = input("Enter the first word: ")
+    b = input("Enter the second word: ")
+    print(mapping(a,b))
+
+if __name__ == "main":
+    main();
